fd47f2b8c803bee94d0f570c6b9b7512446dd7bd
Moin Khan <moinkhan1119rja@gmail.com>
2026-03-19 06:03:12 +0000

    modal updated || 19 March 2026 06:03 AM

--- script/index.js
+++ script/index.js
@@ -94,6 +94,7 @@ const loadModal = (id) => {
 
 const wordModal = (word) => {
   const modal = document.getElementById(`my_modal_1`);
+  modal.innerHTML = ``;
   const synonyms = word.synonyms;
   let synonymsHtml = ``;
   for (const synonym of synonyms) {
