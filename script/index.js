686e1871862e23735641208ac52d8a8310a878c8
Moin Khan <moinkhan1119rja@gmail.com>
2026-03-20 13:03:24 +0000

    null problem solved and button active design added || 20 March 2026 01:03 PM

--- script/index.js
+++ script/index.js
@@ -11,7 +11,7 @@ const displayLesson = (lessons) => {
   for (let lesson of lessons) {
     const lessonDiv = document.createElement(`div`);
     lessonDiv.innerHTML = `
-     <button onclick="levelWords(${lesson.level_no})" class="btn  btn-outline btn-primary"><i class="fa-solid fa-book-open"></i>Lesson - ${lesson.level_no}</button>
+     <button id="lesson-btn-${lesson.level_no}" onclick="levelWords(${lesson.level_no})" class="btn  btn-outline btn-primary lesson-btn"><i class="fa-solid fa-book-open"></i>Lesson - ${lesson.level_no}</button>
     `;
     levelContainer.append(lessonDiv);
   }
@@ -21,34 +21,29 @@ const levelWords = (level) => {
   const url = `https://openapi.programming-hero.com/api/level/${level}`;
   fetch(url)
     .then((r) => r.json())
-    .then((obj) => loadWordCard(obj.data));
+    .then((obj) => {
+      const clickedBtn = document.getElementById(`lesson-btn-${level}`);
+      removeActive();
+      clickedBtn.classList.add("active");
+      loadWordCard(obj.data);
+    });
+};
+
+const removeActive = () => {
+  const inactiveBtn = document.querySelectorAll(".lesson-btn");
+  inactiveBtn.forEach((btn) => btn.classList.remove("active"));
 };
 
 const loadWordCard = (words) => {
   const wordContainer = document.getElementById(`word-container`);
   wordContainer.innerHTML = ``;
-  wordContainer.classList.remove(
-    "flex",
-    "flex-col",
-    "lg:grid",
-    "lg:grid-cols-3",
-    "lg:items-stretch",
-    "gap-4",
-  );
-  wordContainer.classList.add(
-    "flex",
-    "flex-col",
-    "lg:grid",
-    "lg:grid-cols-3",
-    "lg:items-stretch",
-    "gap-4",
-  );
 
   if (words.length == 0) {
     wordContainer.classList.remove(
       "flex",
       "flex-col",
-      "lg:grid",
+      "md:grid",
+      "md:grid-cols-2",
       "lg:grid-cols-3",
       "lg:items-stretch",
       "gap-4",
@@ -58,16 +53,36 @@ const loadWordCard = (words) => {
             <p class="hind text-lg">এই Lesson এ এখনো কোন Vocabulary যুক্ত করা হয়নি।</p>
             <h2 class="text-3xl font-semibold hind">নেক্সট Lesson এ যান</h2>
         </div>`;
+    return;
   }
+
+  wordContainer.classList.remove(
+    "flex",
+    "flex-col",
+    "md:grid",
+    "md:grid-cols-2",
+    "lg:grid-cols-3",
+    "lg:items-stretch",
+    "gap-4",
+  );
+  wordContainer.classList.add(
+    "flex",
+    "flex-col",
+    "md:grid",
+    "md:grid-cols-2",
+    "lg:grid-cols-3",
+    "lg:items-stretch",
+    "gap-4",
+  );
 
   for (let word of words) {
     const wordCard = document.createElement(`div`);
 
     wordCard.innerHTML = `
             <div class="bg-white p-10 text-center space-y-4 h-full rounded-xl">
-            <h1 class="text-3xl font-bold">${word.word}</h1>
+            <h1 class="text-3xl font-bold">${word.word ? word.word : "শব্দ পাওয়া যায়নি!"}</h1>
             <p class="text-xl">Meaning/Pronounciation</p>
-            <p class="hind text-3xl">"${word.meaning} / ${word.pronunciation}"</p>
+            <p class="hind text-3xl">"${word.meaning ? word.meaning : "অর্থ পাওয়া যায়নি!"} / ${word.pronunciation ? word.pronunciation : "উচ্চারণ পাওয়া যায়নি!"}"</p>
             <div class="flex justify-between mt-8">
                 <button onclick="loadModal(${word.id})" class="btn btn-soft btn-primary p-2 rounded-lg">
                     <i class="fa-solid fa-circle-exclamation fa-lg"></i>
@@ -98,19 +113,19 @@ const wordModal = (word) => {
   const synonyms = word.synonyms;
   let synonymsHtml = ``;
   for (const synonym of synonyms) {
-    synonymsHtml += `<p class="bg-[#D7E4EF]/60 py-1 px-2 rounded-lg border-2 border-[#D7E4EF]/80">${synonym}</p>`;
+    synonymsHtml += `<p class="bg-[#D7E4EF]/60 py-1 px-2 rounded-lg border-2 border-[#D7E4EF]/80">${synonym ? synonym : "সমার্থক শব্দ পাওয়া যায়নি!"}</p>`;
   }
   modal.innerHTML = `
           <div class="modal-box">
             <div class="border-3 border-primary/20 p-5 rounded-lg">
-                <h3 class="text-4xl font-bold">${word.word} <span class="hind">(<i
-                            class="fa-solid fa-microphone-lines"></i>:${word.pronunciation})</span></h3>
+                <h3 class="text-4xl font-bold">${word.word ? word.word : "শব্দ পাওয়া যায়নি!"} <span class="hind">(<i
+                            class="fa-solid fa-microphone-lines"></i>:${word.pronunciation ? word.pronunciation : "উচ্চারণ পাওয়া যায়নি!"})</span></h3>
                 <br>
                 <h3 class="text-2xl font-semibold">Meaning</h3>
-                <p class="hind text-2xl">${word.meaning}</p>
+                <p class="hind text-2xl">${word.meaning ? word.meaning : "অর্থ পাওয়া যায়নি!"}</p>
                 <br>
                 <h3 class="text-2xl font-semibold">Example</h3>
-                <p class="text-xl">${word.sentence}</p>
+                <p class="text-xl">${word.sentence ? word.sentence : "উদাহরণ পাওয়া যায়নি!"}</p>
                 <br>
                 <h3 class="text-2xl font-semibold hind">সমার্থক শব্দ গুলো</h3>
                 <div id="syn" class="text-xl flex flex-wrap gap-4 mt-2">
